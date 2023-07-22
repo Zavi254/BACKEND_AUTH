55863557c2209205cf024331345614e168be23cb
Steven Otieno <steveotianga8991@gmail.com>
2023-07-22 03:01:55 +0300

    added cors middleware

--- app.js
+++ app.js
@@ -3,31 +3,20 @@ const app = express();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const auth = require("./auth");
+const cors = require('cors')
 
 // require database connection
 const dbConnect = require("./db/dbConnect");
 const User = require("./db/userModel");
 
 app.use(express.json());
+app.use(cors())
 
 const port = 3000;
 
 // execute database connection
 dbConnect();
 
-// Curb Cores Error by adding a header here
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
